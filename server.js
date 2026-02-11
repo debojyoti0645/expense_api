0c5b492a2a1c27bdd55696586207048e33f88d11
debnath3001 <d.debnath3001@gmail.com>
2026-02-11 16:34:38 +0530

    commit 2

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
+
 app.use('/api/transactions', transactionRoutes);
 
 
@@ -26,6 +27,13 @@ require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+
+//psql 'postgresql://neondb_owner:npg_5gX0imwAJkVz@ep-purple-heart-a16ar86g-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
